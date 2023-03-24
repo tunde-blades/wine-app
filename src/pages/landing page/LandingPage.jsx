4f2d2039112ca6e0ec255066aea8b0bc9bb404de
tunde-blades <82004820+tunde-blades@users.noreply.github.com>
2023-03-24 22:35:51 +0100

    updated

--- src/pages/landing page/LandingPage.jsx
+++ src/pages/landing page/LandingPage.jsx
@@ -83,7 +83,7 @@ export default function LandingPage() {
                 <SwiperSlide><img src={slider1} alt="" /></SwiperSlide>
                 </Swiper>
             </section>
-      <h1 className='font-bold p-3 space'>Top selling</h1>
+      <h1 className='font-bold p-3 space'>Top selling...</h1>
       <section className='space overflow-hidden gap-2  flex flex-wrap'>
           <div className='flex flex-1 w-max flex-row p-2'>
               <div className='box flex flex-col   justify-between bg-white p-2 rounded shadow-md' >
