7f95b3bda1b16b4d9e818aad17405c95f1d6c8e4
tunde-blades <82004820+tunde-blades@users.noreply.github.com>
2023-03-24 23:03:02 +0100

    updated

--- src/pages/landing page/LandingPage.jsx
+++ src/pages/landing page/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import { Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide} from 'swiper/react/swiper-react.js';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import 'swiper/css';
